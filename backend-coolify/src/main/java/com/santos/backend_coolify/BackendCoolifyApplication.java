d5cb60332c0caaa524612e592c789a1ba57b7acf
franciscosilva <franciscosilva@finchsolucoes.com.br>
2026-05-29 16:45:24 -0300

    first commit

---
+++ backend-coolify/src/main/java/com/santos/backend_coolify/BackendCoolifyApplication.java
@@ -0,0 +1,13 @@
+package com.santos.backend_coolify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCoolifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCoolifyApplication.class, args);
+	}
+
+}
---
+++ backend-coolify/src/test/java/com/santos/backend_coolify/BackendCoolifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santos.backend_coolify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCoolifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
